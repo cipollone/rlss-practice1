1e68c0467edb8cd67f2269d3777e5da6952b6ab7
Roberto Cipollone <cipollone.rt@gmail.com>
2023-05-30 12:48:49 +0200

    Added minigrid environment preview.

--- practice1/__main__.py
+++ practice1/__main__.py
@@ -1,9 +1,61 @@
 "Main script: entry point"
 
+import argparse
+from typing import SupportsFloat
+import time
+
+import gymnasium as gym
+
+
+def test_minigrid(env_name: str):
+    """Testing the minigrid environment."""
+
+    # Init
+    env = gym.make(env_name, render_mode="human")
+    observation, info = env.reset()
+    env.render()
+
+    reward: SupportsFloat = 0.0
+    action = None
+    terminated = False
+    truncated = False
+
+    def log():
+        env.render()
+        print("Env step")
+        print("       Action:", action)
+        print("  Observation:", observation)
+        print("       Reward:", reward)
+        print("         Done:", "terminated" if terminated else "truncated")
+        print("         Info:", info)
+        time.sleep(0.1)
+
+    try:
+        while True:
+            action = env.action_space.sample()
+            observation, reward, terminated, truncated, info = env.step(action)
+            log()
+
+            if terminated or truncated:
+                observation, info = env.reset()
+                log()
+    finally:
+        env.close()
+
 
 def main():
     """Main function."""
-    pass
+
+    parser = argparse.ArgumentParser("practical1")
+    sub = parser.add_subparsers(dest="sub", required=True)
+
+    # Develop
+    tester = sub.add_parser("test")
+    tester.add_argument("--minigrid", required=True)
+
+    args = parser.parse_args()
+    if args.sub == "test":
+        test_minigrid(args.minigrid)
 
 
 if __name__ == "__main__":
